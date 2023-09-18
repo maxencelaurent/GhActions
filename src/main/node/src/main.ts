97e1546255bc48b1a4be58d118ec2277162422cc
Maxence <6583528+maxencelaurent@users.noreply.github.com>
2023-09-18 09:21:25 +0200

    dd

---
+++ src/main/node/src/main.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) OPAN concept SA - All Rights Reserved.
+ * THE CONTENTS OF THIS PROJECT ARE PROPRIETARY AND CONFIDENTIAL.
+ * UNAUTHORIZED COPYING, TRANSFERRING OR REPRODUCTION OF THE CONTENT
+ * OF THIS PROJECT, VIA ANY MEDIUM IS STRICTLY PROHIBITED.
+ *
+ * Please read LICENSE file for details
+ */
+
+import * as artifact from '@actions/artifact';
+
+const client = artifact.create();
+
+const path = '/tmp';
+
+async function download(name: string) {
+  try {
+    const result = await client.downloadArtifact(name, path);
+    console.log('Artifact', name, result);
+  } catch {
+    console.log('Not found Artifact', name);
+  }
+}
+
+for (const name of ['homework', 'skhdkhh']) {
+  download(name);
+}
